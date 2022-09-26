5b4ff97dfc6a74c579f2173fe3fbfcaf68e0e365
kevin soriano <kevinsoriano173@gmail.com>
2022-09-26 19:12:16 +0800

    init

---
+++ src/test/java/com/jkngil/pos/discovery/PosAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkngil.pos.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
